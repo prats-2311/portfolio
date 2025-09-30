356689dac2363c15d9d120aa1f160961bb19f729
Prateek Srivastava <prateek.srivastava2311@gmail.com>
2025-09-30 17:18:31 +0530

    Add manifest.json and favicon.svg, fix missing static files

--- app/layout.tsx
+++ app/layout.tsx
@@ -93,9 +93,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        {/* <link rel="icon" href="/favicon.ico" sizes="any" /> */}
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        {/* <link rel="apple-touch-icon" href="/apple-touch-icon.png" /> */}
         <link rel="manifest" href="/manifest.json" />
       </head>
       <body
